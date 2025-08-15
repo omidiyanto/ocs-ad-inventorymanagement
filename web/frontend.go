9905e90b13f3a40128c4c8d5c2876774894e0cc7
O Midiyanto <o.midiyanto@satnusa.com>
2025-08-15 08:36:01 +0700

    fix: favicon

--- web/frontend.go
+++ web/frontend.go
@@ -11,6 +11,7 @@ var FrontendHTML = `
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Delete Computer - OCS Inventory</title>
+  <link rel="icon" type="image/png" href="` + base64LogoOCS + `">
   <style>
     /* Light Mode Palette & Base */
     :root {
