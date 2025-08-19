73f6cda36a33b7221297ac89c3e0f3cb0d7dbc60
O Midiyanto <o.midiyanto@satnusa.com>
2025-08-19 17:12:03 +0700

    update: docker-compose

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t registry.satnusa.com/ocs-ad-inventory-management:latest .
-docker push registry.satnusa.com/ocs-ad-inventory-management:latest
-docker-compose -f docker-compose.yaml down
-docker-compose -f docker-compose.yaml up -d --build
+# docker push registry.satnusa.com/ocs-ad-inventory-management:latest
+docker-compose -f docker-compose.yaml down ocs-ad-inventory
+docker-compose -f docker-compose.yaml up -d --build ocs-ad-inventory
